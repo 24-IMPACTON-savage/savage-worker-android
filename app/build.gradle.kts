c0e18b2cbbfdc3f7521bbbb05b082439064617c5
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-27 19:12:11 +0900

    feat :: add some libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,23 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    // kakao
+    implementation("com.kakao.maps.open:android:2.6.0")
+
+
+    // material
+    implementation("com.google.android.material:material:1.9.0")
+
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
